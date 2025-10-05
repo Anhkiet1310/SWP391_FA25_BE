bc06cafb614ff412e34a22bbc8e9b15f1cc55553
Anhkiet1310 <nguyenvoanhkiet997@gmail.com>
2025-10-05 19:16:32 +0700

    test: ci cd

--- SWP391/Controllers/CarController.cs
+++ SWP391/Controllers/CarController.cs
@@ -62,7 +62,7 @@ namespace SWP391.Controllers
         [HttpGet("test")]
         public async Task<IActionResult> Test()
         {
-            return Ok("test");
+            return Ok("test 2");
         }
     }
 }
