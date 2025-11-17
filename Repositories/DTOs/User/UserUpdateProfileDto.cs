83df5c11b6889650a5072a60e304434c974ead05
nghiahoanglamfer <nghiaduong121@gmail.com>
2025-11-17 09:38:47 +0700

    fix add phone for user

--- Repositories/DTOs/User/UserUpdateProfileDto.cs
+++ Repositories/DTOs/User/UserUpdateProfileDto.cs
@@ -6,6 +6,7 @@ namespace Repositories.DTOs.User
     {
         public string? Email { get; set; }
         public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
         public string? Gender { get; set; }
         public DateTime? Dob { get; set; }
         public string? CCCDFront { get; set; }
